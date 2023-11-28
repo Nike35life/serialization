f83072850114b78c420b5d0c3198fe1d465533a7
Nike35life <142680260+Nike35life@users.noreply.github.com>
2023-11-29 02:15:30 +0300

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -6,12 +6,10 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
+    base: '/News-page/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
   })
