d795aa86b918de18e84bfcb3e45a309cf73e7de5
Nike35life <nik4nok@yandex.com>
2023-11-28 23:34:14 +0300

    serialization and deserialization

---
+++ src/App.vue
@@ -0,0 +1,138 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const activeTab = ref(0);
+
+const initialArray = ref([]);
+initialArray.value = new Array(1000).fill(1).map(() => parseInt(Math.random() * 300));
+
+const dataArray = computed({
+  set: (newValue) => {
+    if (!Array.isArray(newValue)) {
+      initialArray.value = newValue.split(',');
+      return newValue.split(',');
+    }
+    return newValue;
+  },
+  get: () => {
+    return initialArray.value;
+  },
+});
+
+/* UTF-16 Serialisation */
+const serialisation = computed(() => {
+  let serialized = '';
+  for (let i = 0; i < dataArray.value.length; i++) {
+    serialized += String.fromCharCode(dataArray.value[i]);
+  }
+  return serialized;
+});
+const deserialisation = computed(() => {
+  let deserialized = [];
+  for (let i = 0; i < serialisation.value.length; i++) {
+    deserialized.push(serialisation.value.charCodeAt(i));
+  }
+  return deserialized;
+});
+
+/* Equal Serialisation */
+const equalityArray = computed(() => {
+  let equalTemp = Array.from(dataArray.value);
+  let equalityArray = [];
+
+  dataArray.value.forEach((element, index) => {
+    let entires = 0;
+
+    while (equalTemp.includes(element)) {
+      entires++;
+      equalTemp.splice(equalTemp.lastIndexOf(element), 1);
+    }
+    if (entires !== 0) {
+      return equalityArray.push(entires + '.' + element);
+    }
+  });
+
+  return equalityArray;
+});
+const deequalisator = computed(() => {
+  let deserializedArray = [];
+
+  equalityArray.value.forEach((element) => {
+    for (let i = 0; i < parseInt(element.split('.')[0]); i++) {
+      deserializedArray.push(parseInt(element.split('.')[1]));
+    }
+  });
+
+  return deserializedArray;
+});
+</script>
+
+<template>
+  <h1>Serialisation Test by Nikita Shapovalov</h1>
+  <hr />
+  <h2>Initial data</h2>
+  <textarea
+    cols="175"
+    rows="20"
+    v-model="dataArray"></textarea>
+  <div class="center">
+    <button @click="activeTab = 0">UTF-16 Serialisation</button>
+    <button @click="activeTab = 1">Equal Serialisation</button>
+  </div>
+  <!-- utf-16 tab -->
+  <div v-if="activeTab == 0">
+    <h2>Serialised data</h2>
+    <hr />
+    <table border="1">
+      <tr>
+        <td>initial string length</td>
+        <td>serialized string length</td>
+        <td>compress rate</td>
+      </tr>
+      <tr>
+        <td>{{ dataArray.toString().replaceAll(',', '').length }}</td>
+        <td>{{ serialisation.toString().length }}</td>
+        <td>{{ 100 - parseInt((serialisation.toString().length / dataArray.toString().replaceAll(',', '').length) * 100) }}%</td>
+      </tr>
+    </table>
+    <br />
+    <div>{{ serialisation }}</div>
+    <h2>Deserialised data</h2>
+    <div>{{ deserialisation }}</div>
+  </div>
+  <!-- equality tab -->
+  <div v-if="activeTab == 1">
+    <h2>Serialised data</h2>
+    <hr />
+    <table border="1">
+      <tr>
+        <td>initial string length</td>
+        <td>serialized string length</td>
+        <td>compress rate</td>
+      </tr>
+      <tr>
+        <td>{{ dataArray.toString().replaceAll(',', '').length }}</td>
+        <td>{{ equalityArray.toString().length }}</td>
+        <td>{{ 100 - parseInt((equalityArray.toString().length / dataArray.toString().replaceAll(',', '').length) * 100) }}%</td>
+      </tr>
+    </table>
+    <br />
+    <div>{{ equalityArray }}</div>
+    <h2>Deserialised data</h2>
+    <div>{{ deequalisator }}</div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  word-wrap: break-word;
+}
+textarea {
+  width: 100%;
+}
+.center {
+  padding: 15px;
+  text-align: center;
+  background: red;
+}
+</style>
---
+++ src/components/serialisation.vue
@@ -0,0 +1,33 @@
+<script setup>
+let dataArray = new Array(1000).fill(1).map(() => parseInt(Math.random() * 300));
+
+const Serialization = (dataElement) => {
+  let serialized = '';
+  for (let i = 0; i < dataElement.length; i++) {
+    serialized += String.fromCharCode(dataElement[i]);
+  }
+  return serialized;
+};
+
+const Deserialization = (dataSerialized) => {
+  let deserialized = [];
+  for (let i = 0; i < dataSerialized.length; i++) {
+    deserialized.push(dataSerialized.charCodeAt(i));
+  }
+  return deserialized;
+};
+
+const Tests = () => {
+  return dataArray;
+  /* console.log(dataArray); */
+  /* console.log(Serialization(dataArray)); */
+};
+
+module.exports = (function () {
+  return {
+    initialData: dataArray,
+    serialized: Serialization(dataArray),
+    deserialized: Deserialization(Serialization(dataArray)),
+  };
+})();
+</script>
